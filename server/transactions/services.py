2750a4da3b2c25f35b63b700c84f8c00821ea0d6
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-04-21 16:23:24 +0500

    update

---
+++ server/transactions/services.py
@@ -0,0 +1,44 @@
+from django.db import transaction as db_transaction
+from .models import Transaction
+from wallet.models import Wallet
+
+
+def create_transfer(
+    from_wallet: Wallet, to_wallet: Wallet, amount, idempotency_key, description=""
+):
+
+    existing = Transaction.objects.filter(idempotency_key=idempotency_key).first()
+
+    if existing:
+        return existing, False
+
+    with db_transaction.atomic():
+        wallets = Wallet.objects.select_for_update(
+            id__in=[from_wallet.id, to_wallet.id]
+        ).order_by("id")
+
+        sender: Wallet = next(w for w in wallets if w.id == from_wallet.id)
+        reciver: Wallet = next(w for w in wallets if w.id == to_wallet.id)
+
+        if not sender.has_enough_balance(amount):
+            raise ValueError("Balans yetarli emas")
+
+        tx = Transaction.objects.create(
+            txtype=Transaction.TxType.TRANSFER,
+            status=Transaction.TxStatus.PENDING,
+            amount=amount,
+            from_wallet=from_wallet,
+            to_wallet=to_wallet,
+            idempotency_key=idempotency_key,
+            description=description,
+            debit_amount=-amount,
+            credit_amount=+amount,
+        )
+
+        sender.debit(amount)
+        reciver.credit(amount)
+
+        tx.status = Transaction.TxStatus.SUCCESS
+        tx.save(update_fields=["status", "updated_time"])
+
+    return tx, True
