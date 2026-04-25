4a3df302f36d04c7232e925bdf2f22be16e866d1
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-04-25 21:35:33 +0500

    update

--- server/transactions/serializers.py
+++ server/transactions/serializers.py
@@ -1,5 +1,23 @@
 from rest_framework import serializers
+from wallet.models import Wallet
+from decimal import Decimal
 
 
 class CreateTransactionSerializer(serializers.Serializer):
-    pass
+    wallet_id = serializers.UUIDField()
+    amount = serializers.DecimalField(max_digits=18, decimal_places=2)
+    idempotency_key = serializers.CharField(max_length=64)
+
+    def validate_wallet_id(self, value):
+        if not Wallet.objects.filter(id=value).exists():
+            raise serializers.ValidationError("Wallet topilmadi")
+
+    def validate(self, attrs):
+        request = self.context.get("request")
+
+        if request and hasattr(request.user, "wallet"):
+            me_wallet_id = request.user.wallet.id
+            if me_wallet_id == attrs["wallet_id"]:
+                raise serializers.ValidationError(
+                    "ozingizga transaksiya qila olmaysiz "
+                )
--- server/transactions/views.py
+++ server/transactions/views.py
@@ -1,30 +1,48 @@
+from decimal import Decimal
+
+# ================== DJANGO ===================
 from django.shortcuts import render
+
+# ================ MODELS =====================
 from wallet.models import Wallet
+
+# ==================== REST FRAMEWORK =======================
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ValidationError
+
+# ============= SERVICES ===================
 from .services import create_transfer
-from decimal import Decimal
+
+# =========== SERIALIZERS ==============
+from .serializers import CreateTransactionSerializer
 
 
 class CreateTransactionView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
+
+        seriallizer = CreateTransactionSerializer(
+            data=request.data, context={"request": request}
+        )
+        seriallizer.is_valid(raise_exception=True)
+
+        data = seriallizer.validated_data
         from_wallet = self.request.user.wallet
-        to_wallet_id = request.data["wallet_id"]
-        amount = request.data["amount"]
-        to_wallet = Wallet.objects.filter(id=to_wallet_id).first()
-        idempotency_key = request.data["idempotency_key"]
+        to_wallet = Wallet.objects.get(id=data["wallet_to"])
+
+        amount = data["amount"]
+        idempotency_key = data["idempotency_key"]
 
         if to_wallet is None:
             raise ValidationError("user topilmadi")
 
-        result = create_transfer(from_wallet, to_wallet, Decimal(amount), idempotency_key)
+        result = create_transfer(from_wallet, to_wallet, amount, idempotency_key)
 
         print("=" * 50)
         print(result)
         print("=" * 50)
-        
+
         return Response()
